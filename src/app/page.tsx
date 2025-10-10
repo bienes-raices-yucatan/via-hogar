bc928f4d19b4a07fb404f6b0977f1e81c92813b4
Rodrigo <rodrigoaguilarch84@gmail.com>
2025-10-10 12:38:58 +0000

    sigue siendo eso, es gris, CAMBIALO PORFAVOR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -665,7 +665,7 @@ export default function Home() {
   }
 
   return (
-    <div className="flex flex-col min-h-screen bg-background">
+    <div className="flex flex-col min-h-screen bg-white">
         <Header 
             isAdminMode={isAdminMode} 
             setIsAdminMode={setIsAdminMode}
